b9c6254497d5b015c7eb01d4f32cbdd8b442287d
Manny Lee <many221@gmail.com>
2022-04-23 12:30:56 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+my_string = "working is working"
+
+print(len(my_string))
+print(my_string.upper())
